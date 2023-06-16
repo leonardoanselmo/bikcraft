bfd9e8206859a0b82e9f2039e707c243fdcd5d46
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2023-06-16 18:07:32 -0300

    Adicionando JavaScript ao menu-ativo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Teste");
+// Crio uma constante para pegar todos os links do menu
+const links = document.querySelectorAll('.header-menu a');
+
+// Crio a função para fazer o tratamento em cada um dos link
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+    
+  if(url.includes(href)) {
+    link.classList.add("ativo");
+  }  
+}
+
+// Quero ativar essa função em cada um dos links
+links.forEach(ativarLink);
